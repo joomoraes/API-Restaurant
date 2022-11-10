f7e11912bdef9866fe6b1d004be530828cb2ec3c
João Pedro <jp10y@hotmail.com>
2022-11-10 11:25:04 -0400

    #1 Implementation feature of get all data collections in mongoDb

---
+++ Api.Application/Api.Application/Controllers/Outputs/RestaurantList.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Application.Controllers.Outputs
+{
+    public class RestaurantList
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Kitchen { get; set; }
+        public string City { get; set; }
+    }
+}
